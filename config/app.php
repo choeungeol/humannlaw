2a00e5ad951581b8ff9824d47ebf8d7fdedd9952
choeungeol <ceg0416@naver.com>
2016-11-10 21:25:53 +0900

    2016-11-10-2

--- config/app.php
+++ config/app.php
@@ -176,8 +176,7 @@ return [
         \InfyOm\CoreTemplates\CoreTemplatesServiceProvider::class,
         \InfyOm\GeneratorBuilder\GeneratorBuilderServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-
-        Visualplus\Board\ServiceProvider::class,
+        
     ],
 
     /*
