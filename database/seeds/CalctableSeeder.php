b6b6bbf744a25e4e0b3ef1a355ae55b0b0a312b7
choeungeol <ceg0416@naver.com>
2016-11-28 19:42:33 +0900

    2016-11-28

--- database/seeds/CalctableSeeder.php
+++ database/seeds/CalctableSeeder.php
@@ -19,15 +19,15 @@ class CalctableSeeder extends Seeder
             $index = $alpha[$i];
             DB::table('calc_table')->insert([
                 'type' => $index,
-                'mtotal'  => '',     //한달 소정 근로시간
-                'mbreak' => '',     //한달 주휴시간
-                'mover' => '',     //한달 연장 근로시간
-                'mnight' => '',     //한달 야간 근로시간
-                'mwwork' => '',     //한달 휴일 근로시간
-                'mwover' => '',     //한달 휴일 연장
-                'mwnight' => '',     //한달 휴일 야간
-                'mwbt' => '',     //한달 월 연차시간
-                'total' => '',     //총 근로시간
+                'mtotal'  => '0',     //한달 소정 근로시간
+                'mbreak' => '0',     //한달 주휴시간
+                'mover' => '0',     //한달 연장 근로시간
+                'mnight' => '0',     //한달 야간 근로시간
+                'mwwork' => '0',     //한달 휴일 근로시간
+                'mwover' => '0',     //한달 휴일 연장
+                'mwnight' => '0',     //한달 휴일 야간
+                'mwbt' => '0',     //한달 월 연차시간
+                'total' => '0',     //총 근로시간
                 'created_at' => \Carbon\Carbon::now(),
                 'updated_at' => \Carbon\Carbon::now(),
             ]);
