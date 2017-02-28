15380dd3445af129eb8090cb6b2c76719577e438
choeungeol <ceg0416@naver.com>
2017-02-28 14:51:53 +0900

    2017-02-28

--- resources/views/hnl/work/addwork.blade.php
+++ resources/views/hnl/work/addwork.blade.php
@@ -10,10 +10,11 @@
 @section('header_styles')
 
     <meta name="_token" content="{{ csrf_token() }}">
-    <link href="{{ asset('assets/vendors/fullcalendar/css/fullcalendar.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('assets/vendors/fullcalendar/css/fullcalendar.print.css') }}" rel="stylesheet"  media='print' type="text/css">
-    <link href="{{ asset('assets/vendors/iCheck/css/all.css') }}"  rel="stylesheet" type="text/css" />
-    <link href="{{ asset('assets/css/pages/calendar_custom.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/vendors/pickadate/css/default.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/vendors/pickadate/css/default.date.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/vendors/pickadate/css/default.time.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/vendors/airDatepicker/css/datepicker.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/vendors/flatpickrCalendar/css/flatpickr.min.css') }}" rel="stylesheet" type="text/css"/>
 
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/iCheck/css/line/line.css') }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/bootstrap-switch/css/bootstrap-switch.css') }}"/>
@@ -63,89 +64,40 @@
                     <div class="panel-body">
                         <div class="col-lg-8 text-center">
                         <table class="table table-bordered table-condensed">
+                            <caption class="bg-primary">&nbsp;&nbsp;공제근태&nbsp;&nbsp;</caption>
                             <thead>
                             <tr>
-                                <th class="success" rowspan="2">공제근태&nbsp;&nbsp;<br>
-                                    <button type="button" class="btn btn-primary btn-sm" ng-click="addRow()">추가</button>
-                                </th>
                                 <th class="success">공제유형</th>
-                                <td colspan="5">
-                                    <div class="form-group" style="margin-bottom:0px;">
-                                        <label>
-                                            <input type="checkbox" class="square" checked/> 지각
-                                        </label>
-                                        <label>
-                                            <input type="checkbox" class="square"/> 조퇴
-                                        </label>
-                                        <label>
-                                            <input type="checkbox" class="square"/> 외출
-                                        </label>
-                                        <label>
-                                            <input type="checkbox" class="square"/> 결근
-                                        </label>
-                                    </div>
+                                <td>
+                                    <select class="form-control input-sm">
+                                        <option>지각</option>
+                                        <option>조퇴</option>
+                                        <option>외출</option>
+                                        <option>결근</option>
+                                    </select>
                                 </td>
                                 <th class="success">적용일자</th>
                                 <td>
-                                    <input type="date" class="form-control input-sm" />
+                                    <input type="text" class="form-control input-sm" class="disabled-days"/>
                                 </td>
+                                <th class="success">급여반영</th>
+                                <td><input type="checkbox" class="switch" data-size="small"></td>
                             </tr>
                             <tr>
-                                <th class="success">급여반영</th>
-                                <td>여,부</td>
                                 <th class="success">공제시급</th>
-                                <td>자동</td>
+                                <td>!자동출력!</td>
                                 <th class="success">공제시간</th>
-                                <td>__시간</td>
+                                <td><input type="text" class="form-control input-sm"></td>
                                 <th class="success">공제금액</th>
-                                <td>__원</td>
+                                <td><input type="text" class="form-control input-sm"></td>
                             </tr>
                             </thead>
                         </table>
-                            <table class="table table-bordered table-condensed" ng-repeat="wa in work">
-                                <thead>
-                                <tr>
-                                    <th class="success" rowspan="2"><% wa.name %> <% wa.num %><br>
-                                        <button type="button" class="btn btn-warning btn-sm" ng-click="removeRow(wa.name)">삭제</button>
-                                    </th>
-                                    <th class="success">공제유형</th>
-                                    <td colspan="5">
-                                        <div class="form-group" style="margin-bottom:0px;">
-                                            <label>
-                                                <input type="checkbox" class="square" checked/> 지각
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="square"/> 조퇴
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="square"/> 외출
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="square"/> 결근
-                                            </label>
-                                        </div>
-                                    </td>
-                                    <th class="success">적용일자</th>
-                                    <td><input type="text" class="form-control input-sm" /></td>
-                                </tr>
-                                <tr>
-                                    <th class="success">급여반영</th>
-                                    <td>여,부</td>
-                                    <th class="success">공제시급</th>
-                                    <td>자동</td>
-                                    <th class="success">공제시간</th>
-                                    <td>__시간</td>
-                                    <th class="success">공제금액</th>
-                                    <td>__원</td>
-                                </tr>
-                                </thead>
-                            </table>
                         <table class="table table-bordered table-condensed">
                             <thead>
                             <tr>
-                                <th class="success" rowspan="2">추가근무&nbsp;&nbsp;<br>
-                                    <button type="button" class="btn btn-primary btn-sm" ng-click="addRowb()">추가</button>
-                                </th>
+                                <th class="success" rowspan="2">추가근무&nbsp;&nbsp;</th>
+
                                 <th class="success">추가유형</th>
                                 <td colspan="4">
                                     <div class="form-group" style="margin-bottom:0px;">
@@ -178,48 +130,10 @@
                             </tr>
                             </thead>
                         </table>
-                            <table class="table table-bordered table-condensed" ng-repeat="wb in workb">
-                                <thead>
-                                <tr>
-                                    <th class="success" rowspan="2"><% wb.name %> <% wb.num %><br>
-                                        <button type="button" class="btn btn-warning btn-sm" ng-click="removeRowb(wb.name)">삭제</button>
-                                    </th>
-                                    <th class="success">추가유형</th>
-                                    <td colspan="4">
-                                        <div class="form-group" style="margin-bottom:0px;">
-                                            <label>
-                                                <input type="checkbox" class="square" checked/> 추가연장
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="square"/> 추가야간
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="square"/> 휴일근로
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="square"/> 특별근로
-                                            </label>
-                                        </div>
-                                    </td>
-                                    <th class="success">적용일자</th>
-                                    <td><input type="date" class="form-control input-sm" /></td>
-                                </tr>
-                                <tr>
-                                    <th class="success">급여반영</th>
-                                    <td>여,부</td>
-                                    <th class="success">추가 근무시간</th>
-                                    <td>___시간</td>
-                                    <th class="success">추가 근무수당</th>
-                                    <td colspan="2">__원</td>
-                                </tr>
-                                </thead>
-                            </table>
                         <table class="table table-bordered table-condensed">
                             <thead>
                             <tr>
-                                <th class="success" rowspan="2">휴가 휴직&nbsp;&nbsp;<br>
-                                    <button type="button" class="btn btn-primary btn-sm" ng-click="addRowc()">추가</button>
-                                </th>
+                                <th class="success" rowspan="2">휴가 휴직</th>
                                 <th class="success">휴가·휴직 유형</th>
                                 <td colspan="5">
                                     <div class="form-group" style="margin-bottom:0px;">
@@ -251,43 +165,6 @@
                             </tr>
                             </thead>
                         </table>
-                            <table class="table table-bordered table-condensed" ng-repeat="wc in workc">
-                                <thead>
-                                <tr>
-                                    <th class="success" rowspan="2"><% wc.name %> <% wc.num %><br>
-                                        <button type="button" class="btn btn-warning btn-sm" ng-click="removeRowc(wc.name)">삭제</button>
-                                    </th>
-                                    <th class="success">휴가·휴직 유형</th>
-                                    <td colspan="5">
-                                        <div class="form-group" style="margin-bottom:0px;">
-                                            <label>
-                                                <input type="checkbox" class="square" checked/> 병가
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="square"/> 육아휴직
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="square"/> 출산휴가
-                                            </label>
-                                            <label>
-                                                <input type="checkbox" class="square"/> 일반휴직
-                                            </label>
-                                        </div>
-                                    </td>
-                                    <th class="success">적용일자</th>
-                                    <td>범위로 정할 수</td>
-                                </tr>
-                                <tr>
-                                    <th class="success">급여반영</th>
-                                    <td>여,부</td>
-                                    <th class="success"> ㅡ </th>
-                                    <td></td>
-                                    <th class="success"> ㅡ </th>
-                                    <td>반영일수</td>
-                                    <td colspan="2">__원</td>
-                                </tr>
-                                </thead>
-                            </table>
                         </div>
                     </div>
                 </div>
@@ -301,17 +178,19 @@
 {{-- page level scripts --}}
 @section('footer_scripts')
 
-        <!--for calendar-->
-    <script src="{{ asset('assets/vendors/moment/js/moment.min.js') }}"  type="text/javascript"></script>
-    <script src="{{ asset('assets/vendors/fullcalendar/js/fullcalendar.min.js') }}"  type="text/javascript"></script>
-    <script src="{{ asset('assets/vendors/iCheck/js/icheck.js') }}"></script>
-    <script src="{{ asset('assets/js/pages/calendar.js') }}"  type="text/javascript"></script>
-
     <script language="javascript" type="text/javascript" src="{{ asset('assets/vendors/bootstrap-switch/js/bootstrap-switch.js') }}"></script>
     <script language="javascript" type="text/javascript" src="{{ asset('assets/vendors/switchery/js/switchery.js') }}" ></script>
     <script language="javascript" type="text/javascript" src="{{ asset('assets/vendors/bootstrap-maxlength/js/bootstrap-maxlength.js') }}"></script>
     <script language="javascript" type="text/javascript" src="{{ asset('assets/vendors/card/lib/js/jquery.card.js') }}"></script>
     <script language="javascript" type="text/javascript" src="{{ asset('assets/js/pages/radio_checkbox.js') }}"></script>
+
+    <script src="{{ asset('assets/vendors/pickadate/js/picker.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/pickadate/js/picker.date.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/pickadate/js/picker.time.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/flatpickrCalendar/js/flatpickr.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/airDatepicker/js/datepicker.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/airDatepicker/js/datepicker.en.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/js/pages/custom_datepicker.js') }}" type="text/javascript"></script>
 
     <script src="{{ asset('assets/js/hnl/addwork.js') }}" type="text/javascript"></script>
 
